fde1574605cd65635853f6ce70f39380d894a1b5
BelkisHadri <belkis.hadri@thekaustschool.org>
2025-12-18 09:14:17 +0300

    Add files via upload

--- main.f95
+++ main.f95
@@ -1,10 +1,10 @@
 program personal_quiz
     implicit none
-
-    integer :: i
-    integer :: score
-    integer :: userAnswer
-    character(len=30) :: userName    ! NEW: user name
+    open(unit=10, file="quiz_result.txt", status="unknown")
+    integer :: i !score that increases
+    integer :: score !total score to than put into the file 
+    integer :: userAnswer! the input for the questions
+    character(len=30) :: userName   !the user name for than storing it into the file that i make 
 !Modified/Taken from: <Author or GenAI tool name>
 !Based on what I have for now of my questions, I want you to only give me the part that changes the color of my question variables. I want green for the right answer, red for the wrong answer and a color of your choice for the questions>
 !Date accessed/generated: <date>
@@ -17,9 +17,8 @@ program personal_quiz
     character(len=*), parameter :: RESET = achar(27)//"[0m"
 
 !'''end of citation'''
-
    
-   ! The arrays for the questions
+   ! The arrays for the questions. I need to declasre these to have them ready for the user
     character(len=120) :: questions(3) ! 3 quetions in totals
     character(len=60)  :: options(3,4) ! a 2-d array because 3 questions and 4 answers for each 
     integer :: correctAnswers(3) ! 3 correct answers for each of the questions 
@@ -29,23 +28,45 @@ program personal_quiz
     questions(2) = "How long has Belkis been in KAUST?"
     questions(3) = "How good is this code?"
 
+    
     ! Answer options
-    options(1,1) = "Math"
-    options(1,2) = "Computer Science"
-    options(1,3) = "English"
-    options(1,4) = "INS"
+    options(1,1) = "1. Math"
+    options(1,2) = "2. Computer Science"
+    options(1,3) = "3. English"
+    options(1,4) = "4. INS"
 
-    options(2,1) = "3 years"
-    options(2,2) = "11 years"
-    options(2,3) = "7 years"
-    options(2,4) = " 10 years"
+    options(2,1) = "1. 3 years"
+    options(2,2) = "2. 11 years"
+    options(2,3) = "3. 7 years"
+    options(2,4) = "4. 10 years"
 
-    options(3,1) = " Not very good"
-    options(3,2) = " A bit boring"
-    options(3,3) = " Amazing"
-    options(3,4) = " Could be better"
+    options(3,1) = "1. Not very good"
+    options(3,2) = "2. A bit boring"
+    options(3,3) = "3. Amazing"
+    options(3,4) = "4. Could be better"
 
     ! Correct answers
     correctAnswers(1) = 2
     correctAnswers(2) = 2
     correctAnswers(3) = 3
+
+     score = 0
+    print *, "Enter your name:", read *, userName
+
+    print *, "" !i made space to make the code look better 
+    print *, "Welcome to Belkis's quiz dear", (userName) , "Good luck, answer all the questions correctly using only numbers"
+ 
+
+    do 
+    end do
+
+    print *, ""
+    print *, "Final score:", score, "/ 3"
+  
+    write(10,*) "Name:", trim(userName)
+    write(10,*) "Final score:", score, "out of 3"
+    close(10)
+
+    print *, "Your result has been saved."
+
+end program personal_quiz
