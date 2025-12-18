ecc0191c5757266668d224345c33d700f4afc4b6
BelkisHadri <belkis.hadri@thekaustschool.org>
2025-12-18 11:16:43 +0300

    Add files via upload

--- main.f95
+++ main.f95
@@ -5,6 +5,8 @@ program personal_quiz
     integer :: score !total score to than put into the file 
     integer :: userAnswer! the input for the questions
     character(len=30) :: userName   !the user name for than storing it into the file that i make 
+    integer::ioStatus !for code not to crash
+    redo=1
 !Modified/Taken from: <Author or GenAI tool name>
 !Based on what I have for now of my questions, I want you to only give me the part that changes the color of my question variables. I want green for the right answer, red for the wrong answer and a color of your choice for the questions>
 !Date accessed/generated: <date>
@@ -19,36 +21,46 @@ program personal_quiz
 !'''end of citation'''
    
    ! The arrays for the questions. I need to declasre these to have them ready for the user
-    character(len=120) :: questions(3) ! 3 quetions in totals
+    character(len=120) :: questions(4) ! 4 quetions in totals
     character(len=60)  :: options(3,4) ! a 2-d array because 3 questions and 4 answers for each 
     integer :: correctAnswers(3) ! 3 correct answers for each of the questions 
 
     ! Questions in my quiz for each part of the array.
     questions(1) = "What is Belkis's favorite subject?"
     questions(2) = "How long has Belkis been in KAUST?"
-    questions(3) = "How good is this code?"
+    questions(3) = "Who is Belkis's favorite sister?"
+    questions(4) = "How good is this code?"
 
     
-    ! Answer options
+  !questions 1 answers 
     options(1,1) = "1. Math"
     options(1,2) = "2. Computer Science"
     options(1,3) = "3. English"
     options(1,4) = "4. INS"
 
+!questions 2 answers 
     options(2,1) = "1. 3 years"
     options(2,2) = "2. 11 years"
     options(2,3) = "3. 7 years"
     options(2,4) = "4. 10 years"
-
-    options(3,1) = "1. Not very good"
-    options(3,2) = "2. A bit boring"
-    options(3,3) = "3. Amazing"
-    options(3,4) = "4. Could be better"
+ 
+  !question 3 answers  
+    options(3,1) = "She does not have any sisters"
+    options(3,2) = "She does not like any of her sisters"
+    options(3,3) = "She likes them all equally"
+    options(3,4) = "She likes the youngest sister only"
+  
+    !questions 4 answers 
+    options(4,1) = "1. Not very good"
+    options(4,2) = "2. A bit boring"
+    options(4,3) = "3.Could be better
+    options(4,4) = "4. Amazing"
 
     ! Correct answers
     correctAnswers(1) = 2
     correctAnswers(2) = 2
     correctAnswers(3) = 3
+    correctAnswers(4) = 4
 
      score = 0
     print *, "Enter your name:", read *, userName
@@ -57,7 +69,8 @@ program personal_quiz
     print *, "Welcome to Belkis's quiz dear", (userName) , "Good luck, answer all the questions correctly using only numbers"
  
 
-    do 
+    do i = 1, 3
+!questions eing displayed
     end do
 
     print *, ""
